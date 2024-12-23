5f4813eab37f1115e561212d1213f713c4cd0085
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2024-12-23 01:30:24 -0600

    Update StarPlayerInventory.cpp

--- source/game/StarPlayerInventory.cpp
+++ source/game/StarPlayerInventory.cpp
@@ -947,7 +947,7 @@ bool PlayerInventory::checkInventoryFilter(ItemPtr const& items, String const& f
 
   // if an item has a preferred bag, it will go into that bag regardless of any filters
   // if the bag does not exist or no preferred bag is set, this check is skipped
-  auto preferredBag = items->instanceValue("preferredBag", false);
+  auto preferredBag = items->instanceValue("preferredBag", "");
   if (preferredBag) {
     if (config.contains(preferredBag)) {
       if (preferredBag == filterName)
