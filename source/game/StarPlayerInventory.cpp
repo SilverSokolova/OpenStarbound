1aa33c5e2a1fe0ce37884eb0a86b13582d95101a
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2024-12-23 18:04:24 -0600

    Update StarPlayerInventory.cpp

--- source/game/StarPlayerInventory.cpp
+++ source/game/StarPlayerInventory.cpp
@@ -947,7 +947,7 @@ bool PlayerInventory::checkInventoryFilter(ItemPtr const& items, String const& f
   // this check is skipped if no preferred bag is set or (TODO) the bag does not exist
   auto preferredBag = items->instanceValue("preferredBag");
   if (preferredBag) {
-    if (m_bags.contains(preferredBag)) {
+    if (m_bags.keys().contains(preferredBag)) {
       if (preferredBag == filterName)
         return true;
       else
